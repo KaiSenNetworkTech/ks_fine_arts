0240409bfb1ea36e21b527a8f0151bc57297d4de
cxzucc <cxzucc@163.com>
2015-02-03 00:14:58 +0800

    提交接口实现。

--- client/src/main/java/com/kaisen/finearts/service/IQuestionService.java
+++ client/src/main/java/com/kaisen/finearts/service/IQuestionService.java
@@ -1,9 +1,10 @@
 package com.kaisen.finearts.service;
 
+import java.util.List;
+
 import com.kaisen.common.result.CallServiceResult;
 import com.kaisen.finearts.domain.QuestionInfoDO;
 import com.kaisen.finearts.query.QuestionInfoQuery;
-import com.sun.tools.javac.util.List;
 
 public interface IQuestionService {
 	CallServiceResult<Void> insert(QuestionInfoDO questionInfoDO);
--- service/src/main/java/com/kaisen/finearts/service/impl/QuestionServiceImpl.java
+++ service/src/main/java/com/kaisen/finearts/service/impl/QuestionServiceImpl.java
@@ -1,13 +1,96 @@
 package com.kaisen.finearts.service.impl;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.kaisen.common.result.CallServiceResult;
+import com.kaisen.common.result.ResultEnum;
+import com.kaisen.finearts.domain.QuestionInfoDO;
+import com.kaisen.finearts.manager.IQuestionInfoManager;
+import com.kaisen.finearts.query.QuestionInfoQuery;
 import com.kaisen.finearts.service.IQuestionService;
 
 @Service(version = "1.0.0")
 public class QuestionServiceImpl implements IQuestionService {
 	private static Logger logger = LoggerFactory
 			.getLogger(QuestionServiceImpl.class);
+
+	@Resource
+	private IQuestionInfoManager questionInfoManager;
+
+	@Override
+	public CallServiceResult<Void> insert(QuestionInfoDO questionInfoDO) {
+		CallServiceResult<Void> result = new CallServiceResult<Void>();
+		if (questionInfoDO == null) {
+			result.setResultEnum(ResultEnum.PARAMS_ERROR);
+			return result;
+		}
+
+		try {
+			questionInfoManager.insert(questionInfoDO);
+		} catch (Exception e) {
+			logger.error("插入问题失败：" + questionInfoDO, e);
+			result.setResultEnum(ResultEnum.DB_ERROR);
+			return result;
+		}
+
+		return result;
+	}
+
+	@Override
+	public CallServiceResult<Void> update(QuestionInfoDO questionInfoDO) {
+		CallServiceResult<Void> result = new CallServiceResult<Void>();
+		if (questionInfoDO == null) {
+			result.setResultEnum(ResultEnum.PARAMS_ERROR);
+			return result;
+		}
+
+		try {
+			questionInfoManager.update(questionInfoDO);
+		} catch (Exception e) {
+			logger.error("更新问题失败：" + questionInfoDO, e);
+			result.setResultEnum(ResultEnum.DB_ERROR);
+			return result;
+		}
+
+		return result;
+	}
+
+	@Override
+	public CallServiceResult<List<QuestionInfoDO>> query(
+			QuestionInfoQuery questionInfoQuery) {
+		CallServiceResult<List<QuestionInfoDO>> result = new CallServiceResult<List<QuestionInfoDO>>();
+		if (questionInfoQuery == null) {
+			result.setResultEnum(ResultEnum.PARAMS_ERROR);
+			return result;
+		}
+
+		result.setReturnObject(questionInfoManager.query(questionInfoQuery));
+		return result;
+	}
+
+	@Override
+	public CallServiceResult<Void> delete(QuestionInfoDO questionInfoDO) {
+		CallServiceResult<Void> result = new CallServiceResult<Void>();
+		if (questionInfoDO == null || questionInfoDO.getId() == null) {
+			result.setResultEnum(ResultEnum.PARAMS_ERROR);
+			return result;
+		}
+
+		try {
+			questionInfoManager.delete(questionInfoDO);
+		} catch (Exception e) {
+			logger.error("删除问题失败：" + questionInfoDO, e);
+			result.setResultEnum(ResultEnum.DB_ERROR);
+			return result;
+		}
+
+		return result;
+	}
+
 }
